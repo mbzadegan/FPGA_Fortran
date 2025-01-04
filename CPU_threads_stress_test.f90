fde140b194d66bf21844915141ea4332757b1cec
Mohamad Badiezadegan <mbzadegan@gmail.com>
2025-01-04 07:02:06 -0800

    Update CPU_threads_stress_test.f90

--- CPU_threads_stress_test.f90
+++ CPU_threads_stress_test.f90
@@ -4,6 +4,7 @@
 !        gfortran -fopenmp -fno-range-check -o cpu_stress_test CPU_threads_stress_test.f90
 ! Run the program:
 !        ./cpu_stress_test
+! You can install the latest gfortran release by installing the latest GCC.
 
 
 PROGRAM CPUSressTest
